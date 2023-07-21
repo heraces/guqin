f1686cc891138874fa45d4a45ff123c41df74f22
heraces <david.ballesta@hotmail.com>
2023-07-21 19:14:46 +0200

    unity project

---
+++ guqin/Assets/Scripts/Clicking.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicking : MonoBehaviour
+{
+    private Vector3 position;
+    private float width;
+    private float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Move the cube if the screen has the finger moving.
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 pos = touch.position;
+                pos.x = (pos.x - width) / width;
+                pos.y = (pos.y - height) / height;
+                position = new Vector3(-pos.x, pos.y, 0.0f);
+
+                // Position the cube.
+                transform.position = position;
+            }
+
+            if (Input.touchCount == 2)
+            {
+                touch = Input.GetTouch(1);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    // Halve the size of the cube.
+                    transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+                }
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    // Restore the regular size of the cube.
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                }
+            }
+        }
+        }
+}
