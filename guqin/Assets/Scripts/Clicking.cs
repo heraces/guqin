ac0126a80ab46122cd80e45d26e269adc8a95e8f
heraces <david.ballesta@hotmail.com>
2023-07-31 10:52:17 +0200

    no se pero lo voy a subir

--- guqin/Assets/Scripts/Clicking.cs
+++ guqin/Assets/Scripts/Clicking.cs
@@ -8,10 +8,23 @@ public class Clicking : MonoBehaviour
     private float width;
     private float height;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
+
+        // Position used for the cube.
+        position = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    void OnGUI()
+    {
+        // Compute a fontSize based on the size of the screen width.
+        GUI.skin.label.fontSize = (int)(Screen.width / 25.0f);
+
+        GUI.Label(new Rect(20, 20, width, height * 0.25f),
+            "x = " + position.x.ToString("f2") +
+            ", y = " + position.y.ToString("f2"));
     }
 
     // Update is called once per frame
